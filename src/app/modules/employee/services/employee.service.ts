9492c06e2ee1e655f97a90a8db2df699dde4f3ac
rayen-mhenni <mhennirayen2@gmail.com>
2024-08-05 19:47:46 +0100

    fron

--- src/app/modules/employee/services/employee.service.ts
+++ src/app/modules/employee/services/employee.service.ts
@@ -3,24 +3,46 @@ import { StorageService } from '../../../auth/services/storage/storage.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const BASIC_URL = "http://localhost:8082/";
+const BASIC_URL = 'http://localhost:8082/';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeeService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getEmployeeProjectsById():Observable<any>{
-    return this.http.get(BASIC_URL + "api/employee/projects",{
-      headers:this.createAuthorizationHeader()
-    })   
+  getEmployeeProjectsById(): Observable<any> {
+    return this.http.get(BASIC_URL + 'api/employee/projects', {
+      headers: this.createAuthorizationHeader(),
+    });
   }
-  
+
+  getTasksByUserId(id: number): Observable<any> {
+    return this.http.get(BASIC_URL + 'api/v1/task/user/' + id, {
+      headers: this.createAuthorizationHeader(),
+    });
+  }
+  addTask(task: any): Observable<any> {
+    return this.http.post(BASIC_URL + 'api/v1/task', task, {
+      headers: this.createAuthorizationHeader(),
+    });
+  }
+  updateTask(task: any, id: number): Observable<any> {
+    return this.http.put(BASIC_URL + 'api/v1/task/' + id, task, {
+      headers: this.createAuthorizationHeader(),
+    });
+  }
+
+  deleteTask(id: number): Observable<any> {
+    return this.http.delete(BASIC_URL + 'api/v1/task/' + id, {
+      headers: this.createAuthorizationHeader(),
+    });
+  }
+
   private createAuthorizationHeader(): HttpHeaders {
     return new HttpHeaders().set(
-      'Authorization', 'Bearer ' + StorageService.getToken()
-    )
+      'Authorization',
+      'Bearer ' + StorageService.getToken()
+    );
   }
 }
