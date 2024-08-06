06df2b4de5dba272460f231331e244cad483b7d6
oussamaibra <oussamabr58@gmail.com>
2024-08-06 11:05:34 +0100

    push

--- src/app/modules/employee/components/dashboard/dashboard.component.ts
+++ src/app/modules/employee/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { ChartType, ChartDataset, ChartOptions } from 'chart.js';
+import { EmployeeService } from '../../services/employee.service';
 
 interface SideNavToggle {
   screenwidth: number;
@@ -9,23 +10,32 @@ interface SideNavToggle {
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
   innerWidth: any;
   isSideNavCollapsed = false;
   screenWidth = 0;
+  user: any;
+  id: string = '1';
 
   public doughnutChartLabels: string[] = ['To Do', 'In Progress', 'Done'];
   public doughnutChartData: ChartDataset[] = [
     { data: [10, 450, 100], label: 'Series A' },
     { data: [50, 150, 120], label: 'Series B' },
-    { data: [250, 130, 70], label: 'Series C' }
+    { data: [250, 130, 70], label: 'Series C' },
   ];
   public doughnutChartType: ChartType = 'doughnut';
 
+  constructor(private service: EmployeeService) {
+    this.getUser();
+  }
 
-  constructor() {}
+  getUser() {
+    this.service.getEmployeById(this.id).subscribe((res) => {
+      this.user = res;
+    });
+  }
 
   ngOnInit(): void {
     this.innerWidth = window?.innerWidth;
--- src/app/modules/employee/services/employee.service.ts
+++ src/app/modules/employee/services/employee.service.ts
@@ -3,24 +3,30 @@ import { StorageService } from '../../../auth/services/storage/storage.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const BASIC_URL = "http://localhost:8082/";
+const BASIC_URL = 'http://localhost:8082/';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeeService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getEmployeeProjectsById():Observable<any>{
-    return this.http.get(BASIC_URL + "api/employee/projects",{
-      headers:this.createAuthorizationHeader()
-    })   
+  getEmployeeProjectsById(): Observable<any> {
+    return this.http.get(BASIC_URL + 'api/employee/projects', {
+      headers: this.createAuthorizationHeader(),
+    });
   }
-  
+
+  getEmployeById(id: string): Observable<any> {
+    return this.http.get(BASIC_URL + `api/employee/id`, {
+      headers: this.createAuthorizationHeader(),
+    });
+  }
+
   private createAuthorizationHeader(): HttpHeaders {
     return new HttpHeaders().set(
-      'Authorization', 'Bearer ' + StorageService.getToken()
-    )
+      'Authorization',
+      'Bearer ' + StorageService.getToken()
+    );
   }
 }
